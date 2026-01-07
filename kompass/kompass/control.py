f72cd6d74aa3f9bfb78b696c95d9ac7e32a24200
ahr <ahrasheed@gmail.com>
2026-01-07 17:19:21 +0100

    Revert "Trimmed the branch to only the kompass_interfaces sub directory"
    This reverts commit 3c61c12913cd57f896c471ac2d9f82c6d1c1f220.

---
+++ kompass/kompass/control.py
@@ -0,0 +1,17 @@
+from kompass_core.control import (
+    ControllersID,
+    StanleyConfig,
+    DWAConfig,
+    DVZConfig,
+    VisionRGBFollowerConfig,
+    VisionRGBDFollowerConfig,
+)
+
+__all__ = [
+    "ControllersID",
+    "VisionRGBFollowerConfig",
+    "VisionRGBDFollowerConfig",
+    "StanleyConfig",
+    "DWAConfig",
+    "DVZConfig",
+]
