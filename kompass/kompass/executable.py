5cd2bfad36980cdf90dde5ca8bad6c009a428285
ahr <ahrasheed@gmail.com>
2026-02-16 19:17:37 +0100

    Revert "Trimmed the branch to only the kompass_interfaces sub directory"
    This reverts commit 642e51d6fb238b70ff0f23f74c6092499d2a1974.

---
+++ kompass/kompass/executable.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from ros_sugar import executable_main
+from kompass.components import (
+    Controller,
+    ControllerConfig,
+    DriveManager,
+    DriveManagerConfig,
+    MotionServer,
+    MotionServerConfig,
+    Planner,
+    PlannerConfig,
+    LocalMapperConfig,
+    LocalMapper,
+    MapServer,
+    MapServerConfig,
+)
+
+_kompass_components = [
+    Planner,
+    Controller,
+    DriveManager,
+    MotionServer,
+    LocalMapper,
+    MapServer,
+]
+
+_kompass_configs = [
+    PlannerConfig,
+    ControllerConfig,
+    DriveManagerConfig,
+    MotionServerConfig,
+    LocalMapperConfig,
+    MapServerConfig,
+]
+
+
+def main(args=None):
+    """
+    Executable to run a component as a ros node.
+    Used to start a node using Launcher
+
+    # component is any KOMPASS component
+    component = Planner()
+    action = launch_ros.actions.LifecycleNode(
+                                                package="kompass",
+                                                name="node_name",
+                                                executable="executable",
+                                                arguments=component.launch_cmd_args,
+                                            )
+
+    :raises ValueError: If component cannot be started with provided arguments
+    """
+    executable_main(
+        list_of_components=_kompass_components, list_of_configs=_kompass_configs
+    )
